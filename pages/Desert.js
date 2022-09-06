65fded3e52b229943b298c6e96a8796bcf06d4b2
AlexSna <asnatschko@gmail.com>
2022-09-06 12:36:14 +0200

    Add Desert and Veggie Component

---
+++ pages/Desert.js
@@ -0,0 +1,6 @@
+export default function Desert() {
+
+    return (
+    <h1>Desert Rezepte</h1>,
+        <p>alle Nachtisch Rezepte</p>)
+};
---
+++ pages/Veggie.js
@@ -0,0 +1,6 @@
+export default function Veggie() {
+
+    return (
+    <h1>Veggie Rezepte</h1>,
+        <p>alle vegetarischen Rezepte</p>)
+};
